6ce394adfe9dade2bccc48fd36021282c664384d
EduardoSieglitz <eduardosieglitzdasilveira@gmail.com>
2024-04-24 12:06:03 -0300

    Create UmACinquenta.java

---
+++ src/LogicadeProgramaçao/UmACinquenta.java
@@ -0,0 +1,13 @@
+package LogicadeProgramaçao;
+
+public class UmACinquenta {
+    public static void main(String[] args) {
+        for(int i = 0 ; i <= 50 ; i++){
+            if(i == 50) {
+                System.out.print(i + "");
+            }else{
+                System.out.print(i + ",");
+            }
+        }
+    }
+}
